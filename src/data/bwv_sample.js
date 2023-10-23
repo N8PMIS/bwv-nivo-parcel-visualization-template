daf4c405e801b7d573cf5586a1b529d6c7cfaa6e
n8pedigo <nepedigo@crimson.ua.edu>
2023-10-23 14:53:44 -0500

    Adapted from the nivo-parcel template from HTIL repo, sample data and graph editted to fit bw data

---
+++ src/data/bwv_sample.js
@@ -0,0 +1,53 @@
+
+module.exports = [
+    {
+        "id": "beta-waves",
+        "color": "hsl(271, 70%, 50%)",
+        "data": [
+        {
+            "x": "Time",
+            "y": 25
+        },
+        ]
+    },
+    {
+        "id": "alpha-waves",
+        "color": "hsl(287, 70%, 50%)",
+        "data": [
+        {
+            "x": "Time",
+            "y": 10
+        },
+        ]
+    },
+    {
+        "id": "delta-waves",
+        "color": "hsl(260, 70%, 50%)",
+        "data": [
+        {
+            "x": "Time",
+            "y": 3
+        },
+        ]
+    },
+    {
+        "id": "theta-waves",
+        "color": "hsl(240, 70%, 50%)",
+        "data": [
+        {
+            "x": "Time",
+            "y": 6
+        },
+        ]
+    },
+    {
+        "id": "gamma-waves",
+        "color": "hsl(220, 70%, 50%)",
+        "data": [
+        {
+            "x": "Time",
+            "y": 35        
+        },
+        ]
+    }
+]
